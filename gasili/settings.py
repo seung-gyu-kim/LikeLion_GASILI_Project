7065aa65823fe51d1fda6fc6bb2bcb48628b12a6
kimhagyeong <abcd358583@likelion.org>
2019-07-27 19:28:20 +0900

    feedback request

--- gasili/settings.py
+++ gasili/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'gasili.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['gasili/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
