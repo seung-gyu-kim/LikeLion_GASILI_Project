643f6de06002b68f83fbbe546e6e10361264d4a1
이재영 <dldustn14@gmail.com>
2019-07-27 23:12:16 +0900

    board app models 추가

--- gasili/urls.py
+++ gasili/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 import chart.views
 import home.views
 import board.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home.views.index, name='index'),
     path('board_create/',board.views.board_create, name="board_create")
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
