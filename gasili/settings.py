75b365827b75157cdc779c1d5547b228c13613aa
백철민 <ioulo123@naver.com>
2019-07-29 23:59:59 +0900

    first app service

--- gasili/settings.py
+++ gasili/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chart.apps.ChartConfig',
     'home.apps.HomeConfig',
-    'board.apps.BoardConfig'
+    'board.apps.BoardConfig',
 ]
 
 MIDDLEWARE = [
