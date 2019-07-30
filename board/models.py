5a537324de35b073946eb47bf957ecbb8589fdd8
wodud12 <dldustn14@gmail.com>
2019-07-30 16:01:35 +0900

    db 삭제

--- board/models.py
+++ board/models.py
@@ -13,6 +13,7 @@ class Board(models.Model) :
     state = models.CharField(max_length=20)
     userName = models.CharField(max_length=50)
     image = models.ImageField(upload_to='images/')
+    
     def __str__(self) :
         return self.title
     #제목
