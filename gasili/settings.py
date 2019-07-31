2dbb33a64026a1c5e5a74ebf31cf58a9b43e98d7
김도호 <hong7511059@koreatech.ac.kr>
2019-07-31 10:48:24 +0900

    update Allowhost

--- gasili/settings.py
+++ gasili/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 #DEBUG = True
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
