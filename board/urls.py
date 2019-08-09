d6e8bd9e632fa12fdafc10f9e4d588d8dc6af575
김도호 <hong7511059@koreatech.ac.kr>
2019-08-09 22:23:16 +0900

    delete chart app

--- board/urls.py
+++ board/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('create/',views.create, name="board_create"),
     path('test/<int:board_id>',views.test, name="test"),
     path('test/<int:board_id>/create/comment',views.createcomment, name="createcomment"),
+    path('test/chart/<int:board_id>', views.chart, name='chart'),
     path('', views.board, name="board"),
 ]
 
--- gasili/settings.py
+++ gasili/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chart.apps.ChartConfig',
     'home.apps.HomeConfig',
     'board.apps.BoardConfig'
 ]
