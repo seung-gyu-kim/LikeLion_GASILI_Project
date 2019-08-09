9ba5475c56a5472fa2a6d655761541df280f4cc9
Ui Hyeon Kim <yh4268@koreatech.ac.kr>
2019-08-10 03:27:04 +0900

    pagenation

--- board/views.py
+++ board/views.py
@@ -87,9 +87,18 @@ def board(request):
         boards = Board.objects
         boards_list = Board.objects.all()
         paginator = Paginator(boards_list,3)
-        page = request.GET.get('page')
+        if(request.GET.get('page') == None):
+            page=1
+        else:
+            page = int(request.GET.get('page'))
+    
         posts = paginator.get_page(page)
-        return render(request, 'board.html',{'boards' : boards , 'posts':posts})
+        page_range = 5 #페이지 범위 지정 예 1, 2, 3, 4, 5 / 6, 7, 8, 9, 10
+        current_block = math.ceil(page/page_range) #해당 페이지가 몇번째 블럭인가
+        start_block = (current_block-1) * page_range
+        end_block = start_block + page_range
+        p_range = paginator.page_range[start_block:end_block]
+        return render(request, 'board.html',{'boards' : boards , 'posts':posts , 'p_range':p_range , 'page': page})
 
 def createcomment(request, board_id):
         #board = get_object_or_404(Board, pk=board_id) #게시글들에서 하나 뽑음
