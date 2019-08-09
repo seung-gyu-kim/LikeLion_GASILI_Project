e447e43b2c0774936206255d087a9d66f6b6725a
백철민 <ioulo123@naver.com>
2019-08-10 03:01:35 +0900

    service

--- service/admin.py
+++ service/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Board
+from .models import Service
 # Register your models here.
 
-admin.site.register(Board)
+admin.site.register(Service)
--- service/views.py
+++ service/views.py
@@ -1,30 +1,9 @@
-from django.shortcuts import render
-
-def servicecenter(request):
-    return render(request, 'servicecenter.html')
-    
-
-
-import math
-from django.shortcuts import render, get_object_or_404, redirect
-from django.utils import timezone
-from django.core.paginator import Paginator
-from .models import Board
-
+from django.shortcuts import render, get_object_or_404
+from .models import Service
 # Create your views here.
 
-def board_create(request) :
-    return render(request,'board_create.html')
+def servicecenter(request):
+    services = Service.objects
+    return render(request, 'servicecenter.html', {'services' : services})
 
-def test(request, board_id) :
-    board_detail = get_object_or_404(Board, pk=board_id)
-    return render(request,'test.html',{'board' : board_detail})
 
-def board(request):
-    boards = Board.objects
-    boards_list = Board.objects.all()
-    paginator = Paginator(boards_list,3)
-    page = request.GET.get('page')
-    posts = paginator.get_page(page)
-
-    return render(request, 'board.html',{'boards' : boards , 'posts':posts})
